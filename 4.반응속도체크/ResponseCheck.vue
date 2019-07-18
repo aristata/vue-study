e24073c316bf0513538ef6b7c07e5d0b568be10e
SeongMin Jang <smjang@imex.co.kr>
2019-07-18 11:18:22 +0900

    웹팩 감시자 추가

--- 4.반응속도체크/ResponseCheck.vue
+++ 4.반응속도체크/ResponseCheck.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
-    <div id="screen">{{ }}</div>
+    <div id="screen">{{ message }}</div>
     <div>
       <div>평균 시간: {{ }}</div>
-
+      <button @click="onReset">리셋</button>
     </div>
   </div>
 </template>
 <script>
   export default {
     data () {
-      return {}
+      return {
+        result: [],
+        state: 'waiting',
+        message: '클릭해서 시작하세요.'
+      }
     },
-    methods: {}
+    methods: {
+      onReset () {
+
+      }
+    }
   }
 </script>
 <style>
