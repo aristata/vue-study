76bdc86bed71b192cbbf3db1263d60b54d359cdf
SeongMin Jang <smjang@imex.co.kr>
2019-07-18 11:14:00 +0900

    반응속도 체크 시작

---
+++ 4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div id="screen">{{ }}</div>
+    <div>
+      <div>평균 시간: {{ }}</div>
+
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+<style>
+
+</style>
---
+++ 4.반응속도체크/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue' //package.json 에서 설치했던 vue 를 가져온다.
+import ResponseCheck from './ResponseCheck' //vue 파일을 vue 인스턴스에 연결을 해줘야 한다. 그래야 하나의 파일로 합쳐질수 있다.
+
+new Vue(ResponseCheck).$mount('#root')
